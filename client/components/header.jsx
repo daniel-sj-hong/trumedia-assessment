9763b83794bcc699f6b5fcf4647ad2a499738964
Daniel Hong <dan8492@gmail.com>
2022-02-08 20:23:39 -0800

    Cleaned up a lot of the code and moved around a lot of methods. Still in progress with the UI

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -1,89 +1,43 @@
 import React from 'react';
 import { FiMenu } from 'react-icons/fi';
-import axios from 'axios';
+import AppContext from '../lib/context';
 
 export default class Header extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       isModalOn: false,
-      mlbPlayers: [],
-      tempToken: '',
-      playerId: ''
+      mlbPlayers: []
     };
     this.toggle = this.toggle.bind(this);
-    this.getMlbPlayers = this.getMlbPlayers.bind(this);
-    this.getMlbPlayer = this.getMlbPlayer.bind(this);
   }
 
-  toggle() {
-    this.setState(prevState => {
-      return { isModalOn: !prevState.isModalOn };
-    });
-  }
-
-  componentDidMount() {
-    axios.get('/api/mlb/token')
-      .then(results => {
-        console.log('this is the results of componentDidMount: ', results);
-        this.setState({ tempToken: results.data.token });
-        console.log('this is this.state.tempToken after setState: ', this.state.tempToken);
-      })
-      .catch(err => console.error(err));
-  }
-
-  getMlbPlayers() {
-    axios('https://project.trumedianetworks.com/api/mlb/players', {
-      headers: {
-        accept: 'application/json',
-        tempToken: this.state.tempToken
-      }
-    })
-      .then(result => {
-        this.setState({ mlbPlayers: result.data });
-        console.log('this is the result of getMlbPlayers method:', result);
-        console.log('this is mlbPlayers: ', this.state.mlbPlayers);
-      })
-      .catch(err => console.error(err));
-
-    this.toggle();
-  }
-
-  getMlbPlayer(e) {
-    axios(`https://project.trumedianetworks.com/api/mlb/player/${parseInt(e.target.getAttribute('playerid'))}`, {
-      headers: {
-        accept: 'application/json',
-        tempToken: this.state.tempToken
-      }
-    })
-      .then(result => {
-        this.props.updatePlayerSeason(result.data);
-      })
-      .catch(err => console.error(err));
+  toggle(e) {
+    this.setState({ isModalOn: !this.state.isModalOn });
   }
 
   render() {
-
-    const players = this.state.mlbPlayers;
-    const array = players.map(player =>
-      <li key={player.playerId} className="list-item"><a onClick={this.getMlbPlayer} href={`#${player.fullName.replace(' ', '-')}`} playerid={player.playerId}>{player.fullName}</a></li>
-    );
-
     return (
       <>
         <header>
           <div className="header row black">
-            <h1 className="white title" onClick={this.getMlbPlayer}>MLB Stats Tracker</h1>
-            <FiMenu className="align-center white hamburger" onClick={this.getMlbPlayers}/>
+            <h1 className="white title"><a href="#">MLB Stats Tracker</a></h1>
+            <FiMenu className="align-center white hamburger" onClick={this.toggle}/>
           </div>
         </header>
         {this.state.isModalOn &&
         <>
-          <div onClick={this.toggle} className="background absolute"></div>
-          <div className="app-drawer absolute row column align-center">
+          <div onClick={this.toggle} className="background fixed"></div>
+          <div className="app-drawer fixed row column align-center">
             <h2 className="remove-padding">Players</h2>
             <ul className="width-100">
-              {array}
+              {this.props.mlbPlayers.map(player =>
+                <li key={player.playerId} className="list-item">
+                  <a onClick={this.toggle}href={`#player?playerId=${player.playerId}`} playerid={player.playerId}>
+                    {player.fullName}
+                  </a>
+                </li>
+              )}
             </ul>
           </div>
         </>
@@ -92,3 +46,5 @@ export default class Header extends React.Component {
     );
   }
 }
+
+Header.contextType = AppContext;
