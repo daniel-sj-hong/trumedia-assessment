76b2c068725ce8790f4f45f25ba1773b07c528d6
Daniel Hong <dan8492@gmail.com>
2022-02-10 00:09:26 -0800

    Got the map function to successfully display entire season of games data.

--- client/pages/player.jsx
+++ client/pages/player.jsx
@@ -40,7 +40,6 @@ export default class Player extends React.Component {
   }
 
   render() {
-    console.log('this.props: ', this.props);
     console.log('this.state.playerSeason: ', this.state.playerSeason);
     return (
       <>
@@ -63,58 +62,60 @@ export default class Player extends React.Component {
                 </div>
               </div>
 
-              <div className="row justify-center mt-20">
-                <div className="row align-center space-evenly justify-center width-height-background-border">
-                  <div className="mobile-width-30 height-100 row column align-center space-evenly">
-                    <div className="row justify-center">
-                      <h2 className="remove-margin">VS.</h2>
+              {this.state.playerSeason.map(player =>
+                <div className="row justify-center mt-20" key={player.gameDate}>
+                  <div className="row align-center space-evenly justify-center width-height-background-border">
+                    <div className="mobile-width-30 height-100 row column align-center space-evenly">
+                      <div className="row justify-center">
+                        <h2 className="remove-margin">VS.</h2>
+                      </div>
+                      <div className="row width-100 space-evenly">
+                        <h3>{player.opponent}</h3>
+                        <img src={player.opponentImage} />
+                      </div>
+                      <div className="row justify-center">
+                      <h3 className="remove-margin mobile-date-font">Game Date: {format(parse(player.gameDate, 'yyyy-MM-dd HH:mm:ss', new Date()), 'MM/dd/yyyy')}</h3>
+                      </div>
                     </div>
-                    <div className="row width-100 space-evenly">
-                      <h3>{this.state.playerSeason[0].opponent}</h3>
-                      <img src={this.state.playerSeason[0].opponentImage} />
-                    </div>
-                    <div className="row justify-center">
-                    <h3 className="remove-margin mobile-date-font">Game Date: {format(parse(this.state.playerSeason[0].gameDate, 'yyyy-MM-dd HH:mm:ss', new Date()), 'MM/dd/yyyy')}</h3>
-                    </div>
+                    {/* <div className="mobile-width-70 height-100 row justify-center"> */}
+                      <table className="mobile-width-70 height-100 row justify-center align-center">
+                        <tbody>
+                          <tr>
+                            <th>PA:</th>
+                            <td>{player.PA}</td>
+                            <th>K:</th>
+                            <td>{player.K}</td>
+                          </tr>
+                          <tr>
+                            <th>AB:</th>
+                            <td>{player.AB}</td>
+                            <th>HBP:</th>
+                            <td>{player.HBP}</td>
+                          </tr>
+                          <tr>
+                            <th>H:</th>
+                            <td>{player.H}</td>
+                            <th>SF:</th>
+                            <td>{player.SF}</td>
+                          </tr>
+                          <tr>
+                            <th>HR:</th>
+                            <td>{player.HR}</td>
+                            <th>TB:</th>
+                            <td>{player.TB}</td>
+                          </tr>
+                          <tr>
+                            <th>BB:</th>
+                            <td>{player.BB}</td>
+                            <th>RBI:</th>
+                            <td>{player.RBI}</td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    {/* </div> */}
                   </div>
-                  {/* <div className="mobile-width-70 height-100 row justify-center"> */}
-                    <table className="mobile-width-70 height-100 row justify-center align-center">
-                      <tbody>
-                        <tr>
-                          <th>PA:</th>
-                          <td>{this.state.playerSeason[0].PA}</td>
-                          <th>K:</th>
-                          <td>{this.state.playerSeason[0].K}</td>
-                        </tr>
-                        <tr>
-                          <th>AB:</th>
-                          <td>{this.state.playerSeason[0].AB}</td>
-                          <th>HBP:</th>
-                          <td>{this.state.playerSeason[0].HBP}</td>
-                        </tr>
-                        <tr>
-                          <th>H:</th>
-                          <td>{this.state.playerSeason[0].H}</td>
-                          <th>SF:</th>
-                          <td>{this.state.playerSeason[0].SF}</td>
-                        </tr>
-                        <tr>
-                          <th>HR:</th>
-                          <td>{this.state.playerSeason[0].HR}</td>
-                          <th>TB:</th>
-                          <td>{this.state.playerSeason[0].TB}</td>
-                        </tr>
-                        <tr>
-                          <th>BB:</th>
-                          <td>{this.state.playerSeason[0].BB}</td>
-                          <th>RBI:</th>
-                          <td>{this.state.playerSeason[0].RBI}</td>
-                        </tr>
-                      </tbody>
-                    </table>
-                  {/* </div> */}
                 </div>
-              </div>
+              )}
             </>
 
           : <p className="row justify-center">Loading...</p>}
